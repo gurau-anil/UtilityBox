850b91ea335ddddc1890922d3d6a9c1a667747ed
gurau-anil <gurau@gmail.com>
2025-02-04 00:09:42 -0700

    Common regular expressions

---
+++ UtilityBox/CommonRegexPatterns.cs
@@ -0,0 +1,10 @@
+﻿
+namespace UtilityBox
+{
+    public static class CommonRegexPatterns
+    {
+        public const string Email = @"^(?![.-])([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$";
+        
+
+    }
+}
